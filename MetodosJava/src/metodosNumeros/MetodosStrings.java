6de55a4e93973d8f4e76f4affb3df20d866f890e
Dani <danielporras147@outlook.es>
2023-02-03 19:07:57 +0100

    más

--- MetodosJava/src/metodosNumeros/MetodosStrings.java
+++ MetodosJava/src/metodosNumeros/MetodosStrings.java
@@ -5,24 +5,29 @@ import java.util.Comparator;
 import java.util.Random;
 
 public class MetodosStrings {
-	
+
 	/**
-	 * Método que devuele una cadena al azar 
+	 * Método que devuele una cadena al azar
+	 * 
 	 * @param cadenas
 	 * @return
 	 */
 	public static String devolverCadenaAleatorio(String... cadenas) {
 		return cadenas[new Random().nextInt(cadenas.length)];
 	}
+
 	/**
 	 * Método que muestra los elementos de un array
+	 * 
 	 * @param cadenas
 	 */
 	public static void mostrarElementos(String... cadenas) {
-		Arrays.stream(cadenas).forEach(e->System.out.println(e));  
+		Arrays.stream(cadenas).forEach(e -> System.out.println(e));
 	}
+
 	/**
 	 * Método que ordena un array de Strings de la 'a' a la 'z'
+	 * 
 	 * @param cadenas
 	 */
 	public static void ordenaStrings(String... cadenas) {
@@ -30,20 +35,29 @@ public class MetodosStrings {
 		for (String cadena : cadenas) {
 			System.out.println(cadena);
 		}
+		
 	}
-	 
-		
-		
 	
+	public static void separaCadena(String cadena, String separador) {
 
-		public static void ordenaStringsInverso(String... cadenas) {
-			Arrays.sort(cadenas, Comparator.reverseOrder());
-			for(String cadena : cadenas) {
-				System.out.println(cadena);
-			}
+		String[] partes = cadena.split(separador); // partes[0] = "111", partes[1]="Fran"...
+		System.out.println(partes[1] + " tiene " + partes[4] + " euros");
 	}
+
 	
 	
+	
+	
+	/**
+	 * Método que ordena Strings de la 'z' a la 'a'. * @param cadenas
+	 */
+	public static void ordenaStringsInverso(String... cadenas) {
+		Arrays.sort(cadenas, Comparator.reverseOrder());
+		for (String cadena : cadenas) {
+			System.out.println(cadena);
+		}
+	}
+
 	
 	
 	
@@ -51,10 +65,12 @@ public class MetodosStrings {
 	
 	
 	public static void main(String[] args) {
-		//System.out.println(devolverCadenaAleatorio("Hola", "Dani", "ordenador"));
-		//mostrarElementos("cocacola", "bote", "caja", "Dani");
-		ordenaStrings("f","s", "a", "d");
-		ordenaStringsInverso("a","c", "f", "b");
+		// System.out.println(devolverCadenaAleatorio("Hola", "Dani", "ordenador"));
+		// mostrarElementos("cocacola", "bote", "caja", "Dani");
+		//ordenaStrings("f", "s", "a", "d");
+		//ordenaStringsInverso("a", "c", "f", "b");
+		
+		separaCadena("111;Fran;27/07/1976;ES;10000", ";");
 	}
 
 }
